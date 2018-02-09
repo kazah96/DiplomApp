22c9b839c2133dd64feee1b963433eb458ecd1fd
Advocad <advocad15@gmail.com>
2018-02-10 01:32:39 +0500

    тесты

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,74 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const PLACES = [
+  { name: "Орск", zip: "514734" },
+  { name: "Оренбург", zip: "515001" },
+  { name: "Актобе", zip: "610611" },
+  { name: "Москва", zip: "5601538" }
+
+];
+
+
+class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null
+    };
+  }
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL = "http://api.openweathermap.org/data/2.5/weather?id=" +
+      zip +
+      "&appid=6f6c7102b026b65f62443f5f3b6b8783&units=metric";
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ weatherData: json });
+    });
+  }
+  render() {
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    const weather = weatherData.weather[0];
+    console.log(weatherData);
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    return (
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p>Температура: {weatherData.main.temp}°</p>
+        <p>Максимальная: {weatherData.main.temp_max}°</p>
+        <p>Минимальная: {weatherData.main.temp_min}°</p>
+        <p>Скорость ветра: {weatherData.wind.speed} mi/hr</p>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0
+    };
+  }
   render() {
+    const activePlace = this.state.activePlace;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {PLACES.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              this.setState({ activePlace: index });
+            }}
+          >
+            {place.name}
+          </button>
+        ))}
+        <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
       </div>
     );
   }
